c11842ba8db7d9f425411650245d3212a654fff2
APAC\PAT3HC <thanh.PhamXuan@vn.bosch.com>
2022-06-22 18:10:40 +0700

    up

--- routes/index.js
+++ routes/index.js
@@ -5,22 +5,16 @@ export default function routes(app, addon) {
         res.redirect('/atlassian-connect.json');
     });
 
-    // This is an example route used by "generalPages" module (see atlassian-connect.json).
-    // Verify that the incoming request is authenticated with Atlassian Connect.
-    app.get('/hello-world', addon.authenticate(), (req, res) => {
-        // Rendering a template is easy; the render method takes two params: the name of the component or template file, and its props.
-        // Handlebars and jsx are both supported, but please note that jsx changes require `npm run watch-jsx` in order to be picked up by the server.
+    app.get('/hello-world', (req, res) => {
         res.render(
           'hello-world.hbs', // change this to 'hello-world.jsx' to use the Atlaskit & React version
           {
             title: 'ABC'
-            //, issueId: req.query['issueId']
-            //, browserOnly: true // you can set this to disable server-side rendering for react views
           }
         );
     });
 
-    app.get('/main', addon.authenticate(true), (req, res) => {
+    app.get('/main', addon.authenticate(), (req, res) => {
       const {issueKey} = req.query
       getIssueSummary(addon, req, issueKey).then((issueSummary) => {
         res.render(
